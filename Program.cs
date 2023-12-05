3dc5eba403c2279158ad5599968ebe8a60f68c4c
Дмитро Прокопенко <portal200385@gmail.com>
2023-12-05 16:19:59 +0200

    Реалізовано Query3().

--- Program.cs
+++ Program.cs
@@ -67,14 +67,17 @@ namespace Practice_Linq
         // Запит 3
         private static void Query3(List<FootballGame> games)
         {
-            //Query 3: Вивести всі домашні матчі збірної Франції за 2021 рік, де вона зіграла у нічию.
-
-            var selectedGames = games;   // Корегуємо запит !!!
+            // Query 3: Вивести всі домашні матчі збірної Франції за 2021 рік, де вона зіграла у нічию.
+            var selectedGames = games
+                .Where(game => (game.Home_team == "France" && game.Country == "France" && game.Date.Year == 2021 && game.Home_score == game.Away_score))
+                .OrderBy(game => game.Date);
 
             // Перевірка
             Console.WriteLine("\n======================== QUERY 3 ========================");
-
-            // див. приклад як має бути виведено:
+            foreach (var game in selectedGames)
+            {
+                Console.WriteLine($"{game.Date:dd.MM.yyyy} {game.Home_team} - {game.Away_team}, Score: {game.Home_score} - {game.Away_score}, Country: {game.Country}");
+            }
         }
 
         // Запит 4
